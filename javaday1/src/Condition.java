6fd2712ec4b836c5e9a6d82bcc5ac606aee5fc8a
SaiPrashanth0809 <prashanthsai91331@gmail.com>
2024-06-19 10:55:58 +0530

    message

--- javaday1/src/Condition.java
+++ javaday1/src/Condition.java
@@ -1,6 +1,12 @@
 public class Condition {
     public static void main(String[] args) {
-        
+        int a=9;
+        if(a>0){
+            System.out.println("LargeNumber");
+        }
+        else{
+            System.out.println("SmallNumber");
+        }
     }
     
 }

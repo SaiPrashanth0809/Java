0948fc9f5469c36630352ccd87f6d383526034f3
SaiPrashanth0809 <prashanthsai91331@gmail.com>
2024-06-24 10:49:12 +0530

    Prashanth

---
+++ javaday1/src/Stringint.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Stringint {
+    public static void main(String[] args) {
+        System.out.println("==============================");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        int i =  sc.nextInt();
+        for( i=0;i>3;i++){
+            
+        }
+        sc.close();
+    }
+    
+}
---
+++ javaday1/src/Table.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Table {
+    public static void main(String[] args) {
+        int N,I,result;
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        for(I=1;I<=10;I++){
+            System.out.println(N  +  " * "  +  I  +  " = "  +  (N*I));
+        }
+        
+        
+
+    }
+    
+}

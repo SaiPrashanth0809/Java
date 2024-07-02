d0d0492a9f9b07ae6d173e71a87e53b46dc6e5c2
SaiPrashanth0809 <prashanthsai91331@gmail.com>
2024-07-02 10:53:11 +0530

    message

---
+++ javaday1/src/ap.java
@@ -0,0 +1,8 @@
+public class ap {
+    public static void main(String[] args) {
+        car carn = new car("Pavan");
+        System.out.println("This car is belongs to "+ carn.getName());
+        
+    }
+    
+}
---
+++ javaday1/src/car.java
@@ -0,0 +1,17 @@
+public class car{
+    public String name ;
+
+    public car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+  
+}
